37aadcde074f04e37ee91b91234deb2467cb64c3
everydayspring <lee.bom.952@gmail.com>
2024-09-03 16:16:23 +0900

    follower 등록, 조회, 삭제

---
+++ src/main/java/com/example/ondosocial/domain/follower/repository/FollowerRepository.java
@@ -0,0 +1,12 @@
+package com.example.ondosocial.domain.follower.repository;
+
+import com.example.ondosocial.domain.follower.entity.Follower;
+import com.example.ondosocial.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FollowerRepository extends JpaRepository<Follower, Long> {
+    List<Follower> findAllByUser(User user);
+    Follower findOneByUserAndFollower(User user, User follower);
+}

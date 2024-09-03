29ca18ce7b41811c744e91b8e8577c4e4cd43595
everydayspring <lee.bom.952@gmail.com>
2024-09-03 11:46:08 +0900

    project init

---
+++ src/main/java/com/example/ondosocial/OndoSocialApplication.java
@@ -0,0 +1,13 @@
+package com.example.ondosocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndoSocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OndoSocialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ondosocial/OndoSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ondosocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndoSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

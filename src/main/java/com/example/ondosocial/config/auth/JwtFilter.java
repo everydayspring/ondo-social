05e46722f253e613061554a653cf98708d745c10
everydayspring <lee.bom.952@gmail.com>
2024-09-07 17:16:48 +0900

    OncePerRequestFilter 적용

--- src/main/java/com/example/ondosocial/config/auth/JwtFilter.java
+++ src/main/java/com/example/ondosocial/config/auth/JwtFilter.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 import java.util.List;
 
 import jakarta.servlet.*;
-import jakarta.servlet.FilterConfig;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.ondosocial.config.error.AuthErrorCode;
 
@@ -18,72 +22,57 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Component
 @RequiredArgsConstructor
-public class JwtFilter implements Filter {
+public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
     private static final String AUTHORIZATION = "Authorization";
     private static final List<String> PERMIT_ALL_URIS = List.of("/signin", "/signup");
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-    }
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String url = request.getRequestURI();
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        if (!PERMIT_ALL_URIS.contains(url)) {
 
-        String url = httpRequest.getRequestURI();
+            String bearerJwt = request.getHeader(AUTHORIZATION);
 
-        if (PERMIT_ALL_URIS.contains(url)) {
-            chain.doFilter(request, response);
-            return;
+            String jwt = jwtUtil.substringToken(bearerJwt);
+
+            try {
+                // JWT 유효성 검사와 claims 추출
+                Claims claims = jwtUtil.extractClaims(jwt);
+
+                // 사용자 정보를 ArgumentResolver 로 넘기기 위해 HttpServletRequest 에 세팅
+                request.setAttribute("userId", Long.parseLong(claims.getSubject()));
+                request.setAttribute("email", claims.get("email", String.class));
+
+            } catch (SecurityException | MalformedJwtException e) {
+                log.error(AuthErrorCode.INVALID_SIGNATURE.getMessage(), e);
+                throw new ResponseStatusException(
+                        HttpStatus.UNAUTHORIZED, AuthErrorCode.INVALID_SIGNATURE.getMessage());
+            } catch (ExpiredJwtException e) {
+                log.error(AuthErrorCode.EXPIRED_TOKEN.getMessage(), e);
+                throw new ResponseStatusException(
+                        HttpStatus.UNAUTHORIZED, AuthErrorCode.EXPIRED_TOKEN.getMessage());
+            } catch (UnsupportedJwtException e) {
+                log.error(AuthErrorCode.UNSUPPORTED_TOKEN.getMessage(), e);
+                throw new ResponseStatusException(
+                        HttpStatus.BAD_REQUEST, AuthErrorCode.UNSUPPORTED_TOKEN.getMessage());
+            } catch (IllegalArgumentException e) {
+                log.error(AuthErrorCode.EMPTY_CLAIMS.getMessage(), e);
+                throw new ResponseStatusException(
+                        HttpStatus.BAD_REQUEST, AuthErrorCode.EMPTY_CLAIMS.getMessage());
+            } catch (Exception e) {
+                log.error(AuthErrorCode.TOKEN_VERIFICATION_ERROR.getMessage(), e);
+                throw new ResponseStatusException(
+                        HttpStatus.UNAUTHORIZED,
+                        AuthErrorCode.TOKEN_VERIFICATION_ERROR.getMessage());
+            }
         }
-
-        String bearerJwt = httpRequest.getHeader(AUTHORIZATION);
-
-        String jwt = jwtUtil.substringToken(bearerJwt);
-
-        try {
-            // JWT 유효성 검사와 claims 추출
-            Claims claims = jwtUtil.extractClaims(jwt);
-
-            // 사용자 정보를 ArgumentResolver 로 넘기기 위해 HttpServletRequest 에 세팅
-            httpRequest.setAttribute("userId", Long.parseLong(claims.getSubject()));
-            httpRequest.setAttribute("email", claims.get("email", String.class));
-
-            chain.doFilter(request, response);
-        } catch (SecurityException | MalformedJwtException e) {
-            log.error(AuthErrorCode.INVALID_SIGNATURE.getMessage(), e);
-            httpResponse.sendError(
-                    HttpServletResponse.SC_UNAUTHORIZED,
-                    AuthErrorCode.INVALID_SIGNATURE.getMessage());
-        } catch (ExpiredJwtException e) {
-            log.error(AuthErrorCode.EXPIRED_TOKEN.getMessage(), e);
-            httpResponse.sendError(
-                    HttpServletResponse.SC_UNAUTHORIZED, AuthErrorCode.EXPIRED_TOKEN.getMessage());
-        } catch (UnsupportedJwtException e) {
-            log.error(AuthErrorCode.UNSUPPORTED_TOKEN.getMessage(), e);
-            httpResponse.sendError(
-                    HttpServletResponse.SC_BAD_REQUEST,
-                    AuthErrorCode.UNSUPPORTED_TOKEN.getMessage());
-        } catch (IllegalArgumentException e) {
-            log.error(AuthErrorCode.EMPTY_CLAIMS.getMessage(), e);
-            httpResponse.sendError(
-                    HttpServletResponse.SC_BAD_REQUEST, AuthErrorCode.EMPTY_CLAIMS.getMessage());
-        } catch (Exception e) {
-            log.error(AuthErrorCode.TOKEN_VERIFICATION_ERROR.getMessage(), e);
-            httpResponse.sendError(
-                    HttpServletResponse.SC_UNAUTHORIZED,
-                    AuthErrorCode.TOKEN_VERIFICATION_ERROR.getMessage());
-        }
-    }
-
-    @Override
-    public void destroy() {
-        Filter.super.destroy();
+        filterChain.doFilter(request, response);
     }
 }
